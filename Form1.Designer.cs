c676e59fdf61c85181b5d1f981109dcc28c8032d
Just-Shauen <shauenov2007@gmail.com>
2025-05-12 19:30:30 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace AppOrder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnShowOrders = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.listBoxCustomers = new System.Windows.Forms.ListBox();
+            this.dataGridViewOrders = new System.Windows.Forms.DataGridView();
+            this.LabelName = new System.Windows.Forms.Label();
+            this.AgeLabel = new System.Windows.Forms.Label();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOrders)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.SystemColors.Control;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnAdd.ForeColor = System.Drawing.Color.Sienna;
+            this.btnAdd.Location = new System.Drawing.Point(49, 147);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(262, 59);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Registrait";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnShowOrders
+            // 
+            this.btnShowOrders.BackColor = System.Drawing.SystemColors.Control;
+            this.btnShowOrders.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnShowOrders.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.btnShowOrders.Location = new System.Drawing.Point(49, 249);
+            this.btnShowOrders.Name = "btnShowOrders";
+            this.btnShowOrders.Size = new System.Drawing.Size(262, 59);
+            this.btnShowOrders.TabIndex = 1;
+            this.btnShowOrders.Text = "Orders";
+            this.btnShowOrders.UseVisualStyleBackColor = false;
+            this.btnShowOrders.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(161, 49);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(150, 22);
+            this.txtName.TabIndex = 2;
+            this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(161, 86);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(150, 22);
+            this.txtAge.TabIndex = 3;
+            this.txtAge.TextChanged += new System.EventHandler(this.txtAge_TextChanged);
+            // 
+            // listBoxCustomers
+            // 
+            this.listBoxCustomers.FormattingEnabled = true;
+            this.listBoxCustomers.ItemHeight = 16;
+            this.listBoxCustomers.Location = new System.Drawing.Point(350, 42);
+            this.listBoxCustomers.Name = "listBoxCustomers";
+            this.listBoxCustomers.Size = new System.Drawing.Size(422, 164);
+            this.listBoxCustomers.TabIndex = 4;
+            this.listBoxCustomers.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // dataGridViewOrders
+            // 
+            this.dataGridViewOrders.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridViewOrders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewOrders.Location = new System.Drawing.Point(350, 249);
+            this.dataGridViewOrders.Name = "dataGridViewOrders";
+            this.dataGridViewOrders.RowHeadersWidth = 51;
+            this.dataGridViewOrders.RowTemplate.Height = 24;
+            this.dataGridViewOrders.Size = new System.Drawing.Size(422, 167);
+            this.dataGridViewOrders.TabIndex = 5;
+            this.dataGridViewOrders.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // LabelName
+            // 
+            this.LabelName.AutoSize = true;
+            this.LabelName.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LabelName.ForeColor = System.Drawing.Color.Chocolate;
+            this.LabelName.Location = new System.Drawing.Point(44, 42);
+            this.LabelName.Name = "LabelName";
+            this.LabelName.Size = new System.Drawing.Size(82, 29);
+            this.LabelName.TabIndex = 6;
+            this.LabelName.Text = "Name";
+            this.LabelName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // AgeLabel
+            // 
+            this.AgeLabel.AutoSize = true;
+            this.AgeLabel.BackColor = System.Drawing.Color.Transparent;
+            this.AgeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AgeLabel.Location = new System.Drawing.Point(44, 79);
+            this.AgeLabel.Name = "AgeLabel";
+            this.AgeLabel.Size = new System.Drawing.Size(88, 29);
+            this.AgeLabel.TabIndex = 7;
+            this.AgeLabel.Text = "Phone";
+            this.AgeLabel.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // btnExport
+            // 
+            this.btnExport.BackColor = System.Drawing.SystemColors.Control;
+            this.btnExport.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnExport.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.btnExport.Location = new System.Drawing.Point(49, 357);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(262, 59);
+            this.btnExport.TabIndex = 8;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = false;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Chocolate;
+            this.label1.Location = new System.Drawing.Point(291, -3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 29);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Client Order App";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Azure;
+            this.ClientSize = new System.Drawing.Size(835, 470);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.AgeLabel);
+            this.Controls.Add(this.LabelName);
+            this.Controls.Add(this.dataGridViewOrders);
+            this.Controls.Add(this.listBoxCustomers);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnShowOrders);
+            this.Controls.Add(this.btnAdd);
+            this.ForeColor = System.Drawing.Color.Chocolate;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOrders)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnShowOrders;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.ListBox listBoxCustomers;
+        private System.Windows.Forms.DataGridView dataGridViewOrders;
+        private System.Windows.Forms.Label LabelName;
+        private System.Windows.Forms.Label AgeLabel;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.IO;
+
+
+namespace AppOrder
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = "Data Source=customers.db;Version=3;";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            EnsureDatabaseSetup();
+            LoadCustomers();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            int age;
+            if (int.TryParse(txtAge.Text, out age))
+            {
+                string query = "INSERT INTO Customers (Name, Age) VALUES (@Name, @Age)";
+
+                using (SQLiteConnection conn = new SQLiteConnection(connectionString))
+                {
+                    SQLiteCommand cmd = new SQLiteCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@Name", name);
+                    cmd.Parameters.AddWithValue("@Age", age);
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+
+                LoadCustomers();
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите правильный возраст.");
+            }
+        }
+
+        private void LoadCustomers()
+        {
+            listBoxCustomers.Items.Clear();
+            string query = "SELECT Id, Name FROM Customers";
+
+            using (SQLiteConnection conn = new SQLiteConnection(connectionString))
+            {
+                SQLiteCommand cmd = new SQLiteCommand(query, conn);
+                conn.Open();
+                SQLiteDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    string customer = reader["Name"].ToString();
+                    listBoxCustomers.Items.Add(new ListItem
+                    {
+                        Id = Convert.ToInt32(reader["Id"]),                                                                                                                                                                                                                                               
+                        Name = customer
+                    });
+                }
+
+                conn.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBoxCustomers.SelectedItem != null)
+            {
+                ListItem selectedCustomer = (ListItem)listBoxCustomers.SelectedItem;
+                LoadOrders(selectedCustomer.Id);
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, выберите клиента.");
+            }
+
+        }
+        private void EnsureDatabaseSetup()
+        {
+            using (var conn = new SQLiteConnection(connectionString))
+            {
+                conn.Open();
+
+                string createCustomersTable = @"
+            CREATE TABLE IF NOT EXISTS Customers (
+                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                Name TEXT NOT NULL,
+                Age INTEGER NOT NULL
+            );";
+
+                string createOrdersTable = @"
+            CREATE TABLE IF NOT EXISTS Orders (
+                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                CustomerId INTEGER NOT NULL,
+                OrderDetails TEXT NOT NULL,
+                FOREIGN KEY (CustomerId) REFERENCES Customers(Id)
+            );";
+
+                new SQLiteCommand(createCustomersTable, conn).ExecuteNonQuery();
+                new SQLiteCommand(createOrdersTable, conn).ExecuteNonQuery();
+            }
+        }
+
+
+        private void LoadOrders(int customerId)
+        {
+            dataGridViewOrders.Rows.Clear();
+            dataGridViewOrders.Columns.Clear();
+            dataGridViewOrders.Columns.Add("OrderDetails", "Детали заказа");
+
+            using (var conn = new SQLiteConnection(connectionString))
+            {
+                conn.Open();
+                string query = "SELECT OrderDetails FROM Orders WHERE CustomerId = @CustomerId";
+                var cmd = new SQLiteCommand(query, conn);
+                cmd.Parameters.AddWithValue("@CustomerId", customerId);
+
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    dataGridViewOrders.Rows.Add(reader["OrderDetails"].ToString());
+                }
+            }
+        }
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (listBoxCustomers.SelectedItem == null)
+            {
+                MessageBox.Show("Выберите клиента.");
+                return;
+            }
+
+            var selected = (ListItem)listBoxCustomers.SelectedItem;
+            int customerId = selected.Id;
+
+            dataGridViewOrders.Rows.Clear();
+            string query = "SELECT OrderDetails FROM Orders WHERE CustomerId = @CustomerId";
+
+            using (SQLiteConnection conn = new SQLiteConnection(connectionString))
+            {
+                conn.Open();
+                SQLiteCommand cmd = new SQLiteCommand(query, conn);
+                cmd.Parameters.AddWithValue("@CustomerId", customerId);
+
+                SQLiteDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    dataGridViewOrders.Rows.Add(reader["OrderDetails"].ToString());
+                }
+
+                conn.Close();
+            }
+        }
+        public class ListItem
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+
+            public override string ToString()
+            {
+                return Name;
+            }
+        }
+
+
+        private void FilterCustomers(string namePart)
+        {
+            listBoxCustomers.Items.Clear();
+            string query = "SELECT Id, Name FROM Customers WHERE Name LIKE @NamePart";
+
+            using (SQLiteConnection conn = new SQLiteConnection(connectionString))
+            {
+                SQLiteCommand cmd = new SQLiteCommand(query, conn);
+                cmd.Parameters.AddWithValue("@NamePart", "%" + namePart + "%");
+                conn.Open();
+
+                SQLiteDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    listBoxCustomers.Items.Add(new ListItem
+                    {
+                        Id = Convert.ToInt32(reader["Id"]),
+                        Name = reader["Name"].ToString()
+                    });
+                }
+
+                conn.Close();
+            }
+        }
+
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+            FilterCustomers(txtName.Text);
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        
+
+        private void txtAge_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Text files (*.txt)|*.txt";
+            sfd.FileName = "filtered_orders.txt";
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(sfd.FileName))
+                {
+                    // Заголовки колонок
+                    for (int i = 0; i < dataGridViewOrders.Columns.Count; i++)
+                    {
+                        sw.Write(dataGridViewOrders.Columns[i].HeaderText);
+                        if (i < dataGridViewOrders.Columns.Count - 1)
+                            sw.Write("\t"); // Используем табуляцию вместо запятой для разделения
+                    }
+                    sw.WriteLine();
+
+                    // Данные строк (только отфильтрованные заказы)
+                    foreach (DataGridViewRow row in dataGridViewOrders.Rows)
+                    {
+                        if (!row.IsNewRow && row.Visible) // Только видимые строки (отфильтрованные)
+                        {
+                            for (int i = 0; i < dataGridViewOrders.Columns.Count; i++)
+                            {
+                                sw.Write(row.Cells[i].Value?.ToString());
+                                if (i < dataGridViewOrders.Columns.Count - 1)
+                                    sw.Write("\t"); // Используем табуляцию для разделения
+                            }
+                            sw.WriteLine();
+                        }
+                    }
+                }
+
+                // Сообщение об успешном экспорте
+                MessageBox.Show("Успешно экспортированы отфильтрованные заказы в файл " + sfd.FileName);
+            }
+        }
+
+    }
+}
